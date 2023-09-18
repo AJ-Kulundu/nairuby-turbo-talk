23d96fb12b6c85f8f29c608ea763be1f6bdd598a
AJ-Kulundu <jameskulundu@gmail.com>
2023-09-18 22:14:35 +0300

    Todo migration

--- db/migrate/20230918182547_create_todos.rb
+++ db/migrate/20230918182547_create_todos.rb
@@ -1,9 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
-      t.string :todo
-      t.text :description
-
+      t.string :todo, null: false  
+      
       t.timestamps
     end
   end
