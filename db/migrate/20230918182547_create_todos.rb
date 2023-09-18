41b4c08b2146f11de2ccc104bf9ae8928039e7ff
AJ-Kulundu <jameskulundu@gmail.com>
2023-09-18 21:26:32 +0300

    Initial commit

---
+++ db/migrate/20230918182547_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :todos do |t|
+      t.string :todo
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
