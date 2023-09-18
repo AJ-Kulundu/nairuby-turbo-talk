918fe86b97b67a242a7465664a0acec21c3abef0
AJ-Kulundu <jameskulundu@gmail.com>
2023-09-18 22:34:03 +0300

    Todo controller

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,47 @@
+class TodosController < ApplicationController
+
+    def index
+        @todos = Todo.all
+    end
+
+    def show
+        @todo = Todo.find(params[:id])
+    end
+
+    def new
+        @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        if @todo.save
+            redirect_to todo_path(@todo)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        if @todo.update(todo_params)
+            redirect_to todo_path(@todo)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        redirect_to todos_path
+    end
+
+    private
+    def todo_params
+        params.require(:todo).permit(:todo)
+    end
+end
