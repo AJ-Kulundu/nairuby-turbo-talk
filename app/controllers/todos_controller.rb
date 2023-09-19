632c834a7a60c3d0eef86aefc822734380244a97
AJ-Kulundu <jameskulundu@gmail.com>
2023-09-19 12:57:52 +0300

    Destroy turbo stream

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -43,7 +43,10 @@ class TodosController < ApplicationController
     def destroy
         @todo = Todo.find(params[:id])
         @todo.destroy
-        redirect_to todos_path
+        respond_to do |format|
+            format.html {redirect_to todos_path}
+            format.turbo_stream
+        end
     end
 
     private
