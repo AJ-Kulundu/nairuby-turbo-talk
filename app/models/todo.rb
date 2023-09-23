af95902862cceabd1b8daf7a06555f51a918b847
AJ-Kulundu <jameskulundu@gmail.com>
2023-09-23 13:43:42 +0300

    Stream web sockets

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,7 @@
 class Todo < ApplicationRecord
+    # after_create_commit -> { broadcast_append_to "todos" }
+    # after_update_commit -> { broadcast_replace_to "todos" }
+    # after_destroy_commit -> { broadcast_remove_to "todos" }
+
+    broadcasts_to -> (todo) { "todos" }, insert_by: :append
 end
