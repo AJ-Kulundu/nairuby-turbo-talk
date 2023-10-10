a707852ae00073df85e73464448b4df891436905
AJ-Kulundu <jameskulundu@gmail.com>
2023-10-10 15:06:49 +0300

    Webs socket streams

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,17 @@ class Todo < ApplicationRecord
     validates :todo, presence: true
 
     scope :ordered, -> { order(id: :desc)}
+
+    #Broadcasts to the channel
+    # after_create_commit { broadcast_prepend_to "todos" }
+    # after_update_commit { broadcast_replace_to "todos"}
+    # after_destroy_commit { broadcast_remove_to "todos"}
+
+    broadcasts_to -> (todo) {:todos} , inserts_by: :prepend
+    after_create_commit { broadcast_update_to "todos", target: "total", html: Todo.size }
+    after_destroy_commit { broadcast_update_to "todos", target: "total",html: Todo.size  }
+
+    def self.size
+        Todo.count      
+    end
 end
